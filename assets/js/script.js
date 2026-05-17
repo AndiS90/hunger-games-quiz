6a94c68f4d86e5bf9b7bab48808a653076953daf
Andi <andrea.strickland1990@gmail.com>
2026-05-16 23:37:40 -0500

    attempting to push basic added variables

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+var timer = document.querySelector("#timer");
+
+var question = document.querySelector("#question");
+var ansA = document.querySelector("#ans1");
+var ansB = document.querySelector("#ans2");
+var ansC = document.querySelector("#ans3");
+var ansD = document.querySelector("#ans4");
+var ansArray=[ansA, ansB, ansC, ansD];
+
+var validator = document.querySelector("#validator")
+
+var startB = document.querySelector("#start");
+
+var score1 = document.querySelector("#score1");
+var score2 = document.querySelector("#score2");
+var score3 = document.querySelector("#score3");
+var score4 = document.querySelector("#score4");
+var score5 = document.querySelector("#score5");
+var hsArray =[score1, score2, score3, score4, score5];
+
